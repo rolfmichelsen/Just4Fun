2d376305f392f6484f14d8a68a4635ee9908d8d6
Rolf Michelsen <rolf@rolfmichelsen.com>
2016-08-28 22:23:58 +0200

    Add support for reading the input text from a file
    Read the text from file if a filename is specified as a script argument,
    otherwise read input text from standard input.

--- crypto/frequency-analysis.py
+++ crypto/frequency-analysis.py
@@ -47,13 +47,14 @@ def getArguments():
 
     description = 'Output frequency analysis of input text.'
     epilog = '''
-    Read a text from standard input and provide a report on the occurrence frequency of each character.
+    Read a text from file or standard input and provide a report on the occurrence frequency of each character.
     All whitespace characters are normalized to a space before computing frequencies.
     Hint: Use @<filename> to read command line arguments from a file.
     '''
     argParser = ArgumentParser(description=description, epilog=epilog, fromfile_prefix_chars='@')
+    argParser.add_argument('inputfile', action='store', nargs='?', help='name of file containing text to analyse')
     argParser.add_argument('--alpha', dest='alpha', action='store_true', help='only consider alphabetical characters')
-    argParser.add_argument('--alphabet', dest='alphabet', action='store', help='specify characters to include in analysis')
+    argParser.add_argument('--alphabet', metavar='STR', dest='alphabet', action='store', help='specify characters to include in analysis')
     argParser.add_argument('--nocase', dest='nocase', action='store_true', help='ignore character casing')
     argParser.add_argument('-v', '--verbose', dest='verbose', action='store_true', help='more verbose output')
     return argParser.parse_args()
@@ -64,7 +65,13 @@ def getArguments():
 def main():
     args = getArguments()
 
-    text = sys.stdin.read()
+    text = None
+    if args.inputfile:
+        with open(args.inputfile, 'rt') as f:
+            text = f.read()
+    else:
+        text = sys.stdin.read()
+
     if (args.nocase): text = text.lower()
     frequency = frequencyAnalysis(text, alpha=args.alpha, alphabet=args.alphabet)
     outputReport(frequency)
